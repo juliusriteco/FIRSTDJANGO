a5897eff4f040c8d4e8a3f74be2ecae23ef94194
juliusriteco <62749351+juliusriteco@users.noreply.github.com>
2024-09-19 17:47:00 +0200

    update Django

--- tasks/urls.py
+++ tasks/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('newyear/', include("newyear.urls")),
-    path('hello/', include("hello.urls")),
-    path('tasks/', include("tasks.urls"))
+    path("", views.index, name="index"),
+    path("add", views.add, name="add"),
 ]
