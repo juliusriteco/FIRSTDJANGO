6a15c96346ce9afddbe8016b86d7a845351f6c47
juliusriteco <62749351+juliusriteco@users.noreply.github.com>
2024-09-20 15:54:13 +0200

    afgerond en werkend CS50w Django video

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks"
+
 urlpatterns = [
-    path("", views.index, name="index") #in het pad "", index functie in view laden, naam is "index"
+    path("", views.index, name="index"), #in het pad "", index functie in view laden, naam is "index"
+    path("add", views.add, name="add"), #in het pad "", index functie in view laden, naam is "index"
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,36 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
+
+
+
+class NewTaskForm(forms.Form):#functie die een field maakt
+    task = forms.CharField(label="New Task") #maak een input field met characters
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5, initial=3) #maak een input field met integers
+
 # Create your views here.
+
+def index(request): #'index' pagina template, variabele & functies
+    if "tasks" not in request.session:
+        request.session["tasks"] =[] #lijst teruggeven voor in lokale sessie
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+def add(request): #'add' pagina template, variabele & functies
+    if request.method == "POST":
+        form = NewTaskForm(request.POST) #stopt de input van gebruiker van Form in variabele
+        if form.is_valid(): #belangrijk voor server side validation (als nog niet gerefreshed is bijv. )
+            task = form.cleaned_data["task"] #maakt variabele aan met data van inputfield: 'task' uit form
+            request.session["tasks"] += [task] #append, maar dan in lokale sessie 
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks.add.html", { 
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm(), #functie die een form maakt -> Genaamd form
+    })
